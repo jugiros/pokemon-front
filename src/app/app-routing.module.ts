3f8b2ed41ac1f1dcbffc36cadeeb9aec59d902eb
jugiros <31550758+jugiros@users.noreply.github.com>
2022-07-27 10:14:02 -0500

    Creación y diseño del componente para agregar el perfil.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AddProfileComponent} from "./add-profile/add-profile.component";
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    { path: 'add-profile', component: AddProfileComponent }
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderBarComponent } from './components/header-bar/header-bar.component';
+import { AddProfileComponent } from './add-profile/add-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderBarComponent
+    HeaderBarComponent,
+    AddProfileComponent
   ],
   imports: [
     BrowserModule,
