2db35860bd8d9a5d5beb0eb2ebc35f40cdbcdb60
jugiros <31550758+jugiros@users.noreply.github.com>
2022-07-27 09:40:52 -0500

    Creación del componente para el header principal.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './components/header-bar/header-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent
   ],
   imports: [
     BrowserModule,
